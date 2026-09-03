7e28ad965470bff19398a8dfc95426d9bf3d402c
Arunya Pulenthiran <arunyapulenthiran@gmail.com>
2026-09-03 10:29:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 console.log("Welcome to Arunya's portfolio!");
+document.querySelectorAll(".progress").forEach(function(bar) {
+    const level = bar.getAttribute("data-level");
+    bar.style.width = level + "%";
+});
